7bc854a9fd0d950b992d4d86959a0af39c8c3708
owl <kuronc64@gmail.com>
2025-06-30 17:42:58 +0900

    Second Cimittment

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, NavLink } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, NavLink } from "react-router-dom";
 import "./App.css";
 import FadeInSection from "./inView";
 
